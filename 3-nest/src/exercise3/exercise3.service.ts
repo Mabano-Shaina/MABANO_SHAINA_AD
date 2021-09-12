a5fdf0bf27198a3c46512541fb55da1968dd2462
Shaina Mabano <shaina.mabano.20@usjr.edu.ph>
2021-09-12 10:41:47 +0800

    3-nest Assignment Final

--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class Exercise3Service {
     helloWorld(name:string){
-        return "Good Day Sir Dan! I'm "+name;
+        return "Sir, karun pajud ka submit tungods lag hihi. Sorry! "+name;
     }
 
     loopsTriangle(height: number){
@@ -17,5 +17,20 @@ export class Exercise3Service {
             console.log(string);
         }
         return;
+    }
+    primeNumber(parsedNumber: number){
+        if (parsedNumber === 1){
+            return `The number ${parsedNumber} is neither prime nor composite.`;
+        } else if (parsedNumber === 2){
+            return `The number ${parsedNumber} is a prime number.`;
+        }else {
+            for (var x = 2; x < parsedNumber; x++){
+                if (parsedNumber % x === 0){
+                    return `The number ${parsedNumber} is not a prime.`;
+                } else {
+                    return `The number ${parsedNumber} is a prime number.`;
+                }
+            }
+        }
     }
 }
